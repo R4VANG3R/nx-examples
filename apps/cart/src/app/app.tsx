d523f5943f2fd8e64dde79641fa8f0cdcb976be9
Ivon Göttgens <gottgens@smart2it.nl>
2020-07-16 15:12:58 +0200

    Added styled components, styled a div and set up "env" in .babelrc

--- apps/cart/src/app/app.tsx
+++ apps/cart/src/app/app.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import styled from "styled-components";
 
 import '@nx-example/shared/header';
 
 import { CartCartPage } from '@nx-example/cart/cart-page';
 
+const Div = styled.div`
+  background: rebeccapurple;
+`;
+
 export const App = () => {
   return (
     <>
       <nx-example-header />
+      <Div>Foo</Div>
       <Route path="/cart" exact component={CartCartPage} />
     </>
   );
